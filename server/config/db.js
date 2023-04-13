389c7160909c4285f109549edf2f33e78258d7bf
vishwapatel1401 <105445153+vishwapatel1401@users.noreply.github.com>
2023-04-13 18:13:21 -0400

    Delete server directory

--- server/config/db.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.SessionSecret = exports.HostName = exports.LocalURI = exports.RemoteURI = void 0;
-exports.RemoteURI = "mongodb+srv://vishwap1401:<Sv2Gpf4Gul8gio98>@cluster0.vfcegzy.mongodb.net/contact?retryWrites=true&w=majority";
-exports.LocalURI = "mongodb://localhost//contact";
-exports.HostName = "localhost";
-exports.SessionSecret = "WEBD6201SessionSecret";
-//# sourceMappingURL=db.js.map
--- server/config/db.ts
+++
@@ -1,8 +0,0 @@
-"use strict";
-export const RemoteURI = "mongodb+srv://vishwap1401:<Sv2Gpf4Gul8gio98>@cluster0.vfcegzy.mongodb.net/contact?retryWrites=true&w=majority"
-export const LocalURI = "mongodb://localhost//contact"
-
-export const HostName = "localhost";
-//export const HostName = "Mongo DB Atlas";
-
-export const SessionSecret = "WEBD6201SessionSecret";
